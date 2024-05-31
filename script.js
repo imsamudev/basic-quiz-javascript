a3b78a27eb5906d0161d19f2585aebc6ad8d7b5c
SamuDev <samudev01@gmail.com>
2024-05-31 12:51:08 -0300

    Animaciones elementos modal, level, questions, options, showscore

--- script.js
+++ script.js
@@ -26,17 +26,31 @@ function displayQuestion(questionObj) {
 
   // Aplico replace para los saltos de línea en las preguntas
   const formattedQuestion = questionObj.question.replace(/\n/g, "<br>");
-  questionElement.innerHTML = formattedQuestion;
-  optionsElement.innerHTML = "";
 
-  questionObj.options.forEach((option) => {
-    const optionElement = document.createElement("button");
-    optionElement.textContent = option;
-    optionElement.addEventListener("click", () =>
-      checkAnswer(option, questionObj.answer)
-    );
-    optionsElement.appendChild(optionElement);
-  });
+  // animaciones a los elementos de preguntas y opciones
+  questionElement.classList.remove("fade-in");
+  questionElement.classList.add("fade-out");
+  optionsElement.classList.remove("fade-in");
+  optionsElement.classList.add("fade-out");
+
+  setTimeout(() => {
+    questionElement.innerHTML = formattedQuestion;
+    optionsElement.innerHTML = "";
+
+    questionObj.options.forEach((option) => {
+      const optionElement = document.createElement("button");
+      optionElement.textContent = option;
+      optionElement.addEventListener("click", () =>
+        checkAnswer(option, questionObj.answer)
+      );
+      optionsElement.appendChild(optionElement);
+    });
+
+    questionElement.classList.remove("fade-out");
+    questionElement.classList.add("fade-in");
+    optionsElement.classList.remove("fade-out");
+    optionsElement.classList.add("fade-in");
+  }, 300);
 }
 
 // Verificación de respuesta seleccionada por usuario
@@ -62,87 +76,98 @@ function showScore() {
   const tableElement = document.getElementById("responseTable");
   const questionElement = document.getElementById("question");
   const optionsElement = document.getElementById("options");
-  questionElement.textContent = "";
-  optionsElement.innerHTML = "";
 
-  let correctCount = 0;
+  questionElement.classList.remove("fade-in");
+  questionElement.classList.add("fade-out");
+  optionsElement.classList.remove("fade-in");
+  optionsElement.classList.add("fade-out");
 
-  userResponses.forEach((response) => {
-    if (response.userAnswer === response.correctAnswer) {
-      correctCount++;
-    }
+  setTimeout(() => {
+    questionElement.textContent = "";
+    optionsElement.innerHTML = "";
 
-    const row = tableElement.insertRow();
-    row.insertCell(0).textContent = response.question;
-    row.insertCell(1).textContent = response.userAnswer;
-    row.insertCell(2).textContent = response.correctAnswer;
-  });
+    let correctCount = 0;
 
-  const percentage = (correctCount / questions.length) * 100;
-  scoreElement.textContent = `Puntuación final: ${correctCount}/${
-    questions.length
-  } respuestas correctas (${percentage.toFixed(2)}%).`;
+    userResponses.forEach((response) => {
+      if (response.userAnswer === response.correctAnswer) {
+        correctCount++;
+      }
 
-  // Ejecutar tabla de puntuación al finalizar el quizz.
-  tableElement.style.display = "block";
+      const row = tableElement.insertRow();
+      row.insertCell(0).textContent = response.question;
+      row.insertCell(1).textContent = response.userAnswer;
+      row.insertCell(2).textContent = response.correctAnswer;
+    });
 
-  // Sección de corrección
-  const correctionAccordion = document.getElementById("correctionAccordion");
-  const toggleAccordionButton = document.getElementById("toggleAccordion");
+    const percentage = (correctCount / questions.length) * 100;
+    scoreElement.textContent = `Puntuación final: ${correctCount}/${
+      questions.length
+    } respuestas correctas (${percentage.toFixed(2)}%).`;
 
-  // Visibilidad del acordeón
-  toggleAccordionButton.addEventListener("click", () => {
-    if (correctionAccordion.style.display === "none") {
-      correctionAccordion.style.display = "block";
-    } else {
-      correctionAccordion.style.display = "none";
-    }
-  });
-  // Contenido acordeón
-  correctionAccordion.style.display = "none";
+    // Ejecutar tabla de puntuación al finalizar el quizz.
+    tableElement.style.display = "block";
 
-  // Mostrar la sección de corrección al finalizar el quiz
-  const correctionElement = document.getElementById("correction");
-  correctionElement.style.display = "block";
+    // Sección de corrección
+    const correctionAccordion = document.getElementById("correctionAccordion");
+    const toggleAccordionButton = document.getElementById("toggleAccordion");
 
-  userResponses.forEach((response) => {
-    if (response.userAnswer !== response.correctAnswer) {
-      const correctionItem = document.createElement("div");
-      correctionItem.innerHTML = `
-      <div id ="item">
-      <p class="correctionText"><strong>Pregunta:</strong> ${
-        response.question
+    // Visibilidad del acordeón
+    toggleAccordionButton.addEventListener("click", () => {
+      if (correctionAccordion.style.display === "none") {
+        correctionAccordion.style.display = "block";
+      } else {
+        correctionAccordion.style.display = "none";
+      }
+    });
+    // Contenido acordeón
+    correctionAccordion.style.display = "none";
+
+    // Mostrar la sección de corrección al finalizar el quiz
+    const correctionElement = document.getElementById("correction");
+    correctionElement.style.display = "block";
+
+    userResponses.forEach((response) => {
+      if (response.userAnswer !== response.correctAnswer) {
+        const correctionItem = document.createElement("div");
+        correctionItem.innerHTML = `
+        <div id ="item">
+        <p class="correctionText"><strong>Pregunta:</strong> ${
+          response.question
+        }</p>
+      <p class="correctionText"><strong>Respuesta seleccionada:</strong> ${
+        response.userAnswer
       }</p>
-    <p class="correctionText"><strong>Respuesta seleccionada:</strong> ${
-      response.userAnswer
-    }</p>
-    <p class="correctionText"><strong>Respuesta correcta:</strong> ${
-      response.correctAnswer
-    }</p>
-    <p class="correctionText"><strong>Explicación:</strong> ${
-      questions.find((question) => question.question === response.question)
-        .explanation
-    }</p>
-    </div>
-  `;
-      correctionAccordion.appendChild(correctionItem);
-    }
-  });
+      <p class="correctionText"><strong>Respuesta correcta:</strong> ${
+        response.correctAnswer
+      }</p>
+      <p class="correctionText"><strong>Explicación:</strong> ${
+        questions.find((question) => question.question === response.question)
+          .explanation
+      }</p>
+      </div>
+    `;
+        correctionAccordion.appendChild(correctionItem);
+      }
+    });
+  }, 800);
 }
 
 // Agrego función para modal
 document.addEventListener("DOMContentLoaded", (event) => {
   var modal = document.getElementById("introModal");
   var levelSelection = document.getElementById("levelSelection");
-
   var startQuiz = document.getElementById("startQuiz");
 
+  modal.classList.add("fade-in");
   modal.style.display = "flex";
 
   // Al pulsar el logo muestro la selección del nivel
   startQuiz.onclick = function () {
-    modal.style.display = "none";
-    levelSelection.style.display = "block";
+    setTimeout(() => {
+      modal.style.display = "none";
+      levelSelection.style.display = "block";
+      levelSelection.classList.add("fade-in");
+    }, 500);
   };
 
   var startBeginnerQuiz = document.getElementById("startBeginnerQuiz");
@@ -150,14 +175,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   // Nivel principiante
   startBeginnerQuiz.onclick = function () {
-    levelSelection.style.display = "none";
-    startQuizFunction("questions.json");
+    levelSelection.classList.remove("fade-in");
+    levelSelection.classList.add("fade-out");
+    setTimeout(() => {
+      levelSelection.style.display = "none";
+      startQuizFunction("questions.json");
+    }, 1000);
   };
 
   // Nivel intermedio
   startIntermediateQuiz.onclick = function () {
-    levelSelection.style.display = "none";
-    startQuizFunction("questions_intermediate-level.json");
+    levelSelection.classList.remove("fade-in");
+    levelSelection.classList.add("fade-out");
+    setTimeout(() => {
+      levelSelection.style.display = "none";
+      startQuizFunction("questions_intermediate-level.json");
+    }, 1000);
   };
 });
 
